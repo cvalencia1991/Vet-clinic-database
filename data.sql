f3ee2bb60302aad6c0f5957748114d6df9ac38a2
Kelvin Ben <kevoben14@gmail.com>
2022-12-13 01:08:03 +0300

    Add data to our database

--- data.sql
+++ data.sql
@@ -185,6 +185,8 @@ SELECT * FROM (SELECT id FROM animals) animal_ids,
 generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
 
 
-
+insert into owners (full_name, email) 
+select 'Owner ' || generate_series(1,2500000), 
+'owner_' || generate_series(1,2500000) || '@mail.com';
 
 
